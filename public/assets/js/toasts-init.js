84bb2fb3992e72e455465a97f5a6cca19b344d46
Daniel Huggins <contactdanielhuggins@gmail.com>
2021-07-28 21:54:31 +0100

    orm in place, moving on to notice and error handling

---
+++ public/assets/js/toasts-init.js
@@ -0,0 +1,19 @@
+var ready= (callback) => {
+    if (document.readyState != 'loading') callback();
+    else document.addEventListener("DOMContentLoaded", callback);
+}
+
+ready(() => {
+    var toastElList = [].slice.call(document.querySelectorAll('.toast'))
+    var toastList = toastElList.map(function (toastEl) {
+        return new bootstrap.Toast(toastEl, { autohide: false })
+    })
+
+    toastList.forEach(toast => {
+        toast.show();
+    });
+
+    document.querySelectorAll
+
+
+});
